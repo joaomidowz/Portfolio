1d6124dd96ab5472bcc67a35faf47fb0346af13a
João Gabriel <jgcustodio2005@gmail.com>
2025-01-28 09:12:33 -0300

    Adicionando Swiper e Carroussel de projetos

--- src/pages/Portifolio.tsx
+++ src/pages/Portifolio.tsx
@@ -1,9 +1,29 @@
 import "../App.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-function Portifolio () {
+// Importando Estilo do Swiper
+import 'swiper/css';
+
+import 'swiper/swiper-bundle.css'; // Estilos principais do Swiper
+
+function Portifolio() {
     return (
         <div className="w-screen h-screen md:w-auto bg-secondary font-poppins text-primary p-5">
-
+            <div className="p-20 ">
+            <p className="text-2xl mb-2.5">Projetos:</p>
+            <Swiper
+                spaceBetween={55}
+                slidesPerView={3}
+                loop={true}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide><img src="Rectangle 3.png" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="Rectangle 3.png" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="Rectangle 3.png" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="Rectangle 3.png" alt="" /></SwiperSlide>
+            </Swiper>
+            </div>
         </div>
 
     )
