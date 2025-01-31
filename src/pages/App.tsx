97f5a29855b4881208fe4027a179d2a4b979ccfd
João Gabriel <jgcustodio2005@gmail.com>
2025-01-31 09:57:50 -0300

    Ajustando botões para Celular

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -13,20 +13,20 @@ const scrollToSection = (id: string) => {
 
     <div className="w-screen h-screen bg-primary p-5 font-poppins ">
       <header className='p-3 max-sm:p-0.5'>
-        <nav className='flex justify-end gap-10 max-mobile:gap-3 max-sm:gap-2 max-phone:text-xs'>
-          <button onClick={() => scrollToSection('about')} className='px-5 py-4 text-primary bg-secondary rounded-lg max-mobile:px-3 max-mobile:py-2'>Sobre</button>
-          <button onClick={() => scrollToSection('portfolio')} className='px-5 py-4 text-primary bg-secondary rounded-lg  max-mobile:px-3 max-mobile:py-2'>Projetos</button>
+        <nav className='flex justify-end gap-10 max-mobile:gap-3 max-sm:gap-2 max-phone:text-xs max-phone:justify-between max-phone:px-1'>
+          <button onClick={() => scrollToSection('about')} className='px-5 py-4 text-primary bg-secondary rounded-lg max-mobile:px-5 max-mobile:py-2'>Sobre</button>
           <button onClick={() => scrollToSection('tech')} className='px-5 py-4 text-primary bg-secondary rounded-lg max-mobile:px-3 max-mobile:py-2'>Tecnologias</button>
+          <button onClick={() => scrollToSection('portfolio')} className='px-5 py-4 text-primary bg-secondary rounded-lg  max-mobile:px-3 max-mobile:py-2'>Projetos</button>
         </nav>
       </header>
 
-      <div className='mt-20 -ml-20 flex flex-col items-center max-phone:ml-auto'>
+      <div className='mt-20 -ml-20 flex flex-col items-center max-phone:ml-auto max-phone:mt-10'>
 
-      <div className="w-full max-w-4xl flex justify-start mr-32 max-laptop:ml-56 max-tablet:-mr-20 max-mobile:-mr-0 max-sm:mr-6 max-phone:ml-24">
+      <div className="w-full max-w-4xl flex justify-start mr-32 max-laptop:ml-56 max-tablet:-mr-20 max-mobile:-mr-0 max-sm:mr-6 max-phone:ml-12">
           <p className="text-4xl text-secondary font-regular max-laptop:text-3xl max-tablet:text-2xl max-mb:text-xl max-phone:text-base">Olá, me chamo João 👋</p>
         </div>
 
-        <div className='flex justify-self-center items-baseline  gap-4 ml-28 -mt-10 drop-shadow-lg  max-laptop:ml-20 max-tablet:-mt-6 max-sm:mt-0 max-sm:ml-10 max-sm:gap-2 max-phone:gap-1 max-phone:ml-3'>
+        <div className='flex justify-self-center items-baseline  gap-4 ml-28 -mt-10 drop-shadow-lg  max-laptop:ml-20 max-tablet:-mt-6 max-sm:-mt-4 max-sm:ml-10 max-sm:gap-2 max-phone:gap-1 max-phone:ml-2'>
           <p className='text-Header text-secondary drop-shadow-xl font-extrabold max-laptop:text-Header2 max-tablet:text-Header3 max-sm:text-7xl max-phone:text-5xl'>Fullstack</p>
           <a href="https://bit.ly/4gikrSU"><img src="/LinkedIn.svg" alt="Linkedin" className='h-28 w-28 max-laptop:h-20 max-laptop:w-20 max-tablet:h-16 max-tablet:w-16 max-sm:w-12 max-sm:pt-5 max-phone:w-auto max-phone:h-12 ' /></a>
           <a href="https://github.com/joaomidowz"><img src="/GitHub.svg" alt="GitHub" className='h-28 w-28 max-laptop:h-20 max-laptop:w-20 max-tablet:h-16 max-tablet:w-16 max-sm:w-12 max-sm:pt-5 max-phone:w-auto max-phone:h-12 ' /></a>
