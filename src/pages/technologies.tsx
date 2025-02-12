789b0d9a1c0d0c3cb4ad703cdbdf56088317e4d5
João Gabriel <jgcustodio2005@gmail.com>
2025-02-12 16:38:12 -0300

    Ajeitando Header de novo alinhando.

--- src/pages/technologies.tsx
+++ src/pages/technologies.tsx
@@ -4,12 +4,12 @@ function Tech() {
 
     return (
 
-        <div className="w-screen h-screen md:w-auto bg-secondary font-poppins text-primary p-5 max-phone:flex max-phone:flex-col max-phone:place-items-center max-phone:h-100 max-phone:rounded-t-3xl max-phone:mb-8px max-phone:shadow-phone">
+        <div className="w-screen h-screen md:w-auto bg-secondary font-poppins text-primary p-5  max-phone:h-100 max-phone:rounded-t-3xl max-phone:mb-8px max-phone:shadow-phone">
 
-            <div className='flex '>
-                <h1 className='font-black text-2xl p-14 max-mobile:text-xl max-phone:text-lg' >Técnologias</h1>
-            </div>
-            <div className='flex justify-center items-center h-full -mt-32'>
+
+            <h1 className='font-black text-2xl p-14 max-mobile:text-xl max-phone:text-lg max-phone:p-6 max-phone:mb-8' >Técnologias</h1>
+
+            <div className='flex justify-center '>
                 <img src="Elipse.svg" alt="elipse" className='max-w-[450px] max-h-[450px]  max-mobile:max-w-[300px] max-mobile:max-h-[300px] max-phone:max-h-[250px] max-phone:max-h-[250px]' />
             </div>
         </div>
