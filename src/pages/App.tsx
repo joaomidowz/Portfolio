047a806544fe7cab97affbc1853cf0b1d52db397
João Gabriel <jgcustodio2005@gmail.com>
2025-02-12 16:05:32 -0300

    Adicionando animação na mão.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,14 @@
 import '../App.css'
+import { motion } from "framer-motion";
 
 function App() {
 
-const scrollToSection = (id: string) => {
-  const section = document.getElementById(id);
-  if (section) {
-    section.scrollIntoView({behavior: "smooth"})
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" })
+    }
   }
-}
 
   return (
 
@@ -22,8 +23,20 @@ const scrollToSection = (id: string) => {
 
       <div className='mt-20 -ml-20 flex flex-col items-center max-phone:ml-auto max-phone:mt-10'>
 
-      <div className="w-full max-w-4xl flex justify-start mr-32 max-laptop:ml-56 max-tablet:-mr-20 max-mobile:-mr-0 max-sm:mr-6 max-phone:ml-20 max-sms:ml-10">
-          <p className="text-4xl text-secondary font-regular max-laptop:text-3xl max-tablet:text-2xl max-mb:text-xl max-phone:text-base">Olá, me chamo João 👋</p>
+        <div className="w-full max-w-4xl flex justify-start mr-32 max-laptop:ml-56 max-tablet:-mr-20 max-mobile:-mr-0 max-sm:mr-6 max-phone:ml-20 max-sms:ml-10">
+          <p className="text-4xl text-secondary font-regular max-laptop:text-3xl max-tablet:text-2xl max-mb:text-xl max-phone:text-base">Olá, me chamo João
+            <motion.div
+              initial={{ rotate: 0 }}
+              animate={{ rotate: [50, 10, 30] }} 
+              transition={{
+                duration: 0.6,
+                repeat: Infinity,
+                repeatType: "reverse", 
+              }}
+              className="inline-block"
+            >
+              👋
+            </motion.div></p>
         </div>
 
         <div className='flex justify-self-center items-baseline  gap-4 ml-28 -mt-10 drop-shadow-lg  max-laptop:ml-20 max-tablet:-mt-6 max-sm:-mt-4 max-sm:ml-10 max-sm:gap-2 max-phone:gap-1 max-phone:ml-2'>
@@ -33,7 +46,7 @@ const scrollToSection = (id: string) => {
           <a href="https://bit.ly/3Cv2uPK"><img src="/Google Docs.svg" alt="Google Docs" className='h-28 w-28 max-laptop:h-20 max-laptop:w-20 max-tablet:h-16 max-tablet:w-16 max-sm:w-12 max-sm:pt-5 max-phone:w-auto max-phone:h-12 ' /></a>
         </div>
         <div className='flex justify-self-center items-baseline drop-shadow-xl -mt-[95px] max-laptop:-mt-20 max-tablet:-mt-14 max-sm:mt-0'>
-        <p className='text-Header  text-secondary drop-shadow-xl font-extrabold max-laptop:text-Header2 max-tablet:text-Header3 max-sm:text-7xl max-phone:text-5xl'>Developer</p>
+          <p className='text-Header  text-secondary drop-shadow-xl font-extrabold max-laptop:text-Header2 max-tablet:text-Header3 max-sm:text-7xl max-phone:text-5xl'>Developer</p>
         </div>
       </div>
     </div>
