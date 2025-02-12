73902a7ab29490a5bab2f872e189db6c8a1ca8b4
João Gabriel <jgcustodio2005@gmail.com>
2025-02-12 16:45:24 -0300

    Ajeitando Header de novo alinhando. Alinhando de novo.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,9 +6,9 @@ function About() {
 
         <div className="w-screen h-screen md:w-auto bg-primary font-poppins text-secondary max-phone:h-100 max-phone:rounded-t-3xl max-phone:shadow-phone">
             <div className=''>
-                <p className='font-black text-2xl max-mobile:text-xl max-phone:text-lg p-16  max-mobile:text-xl max-tablet:p-10 max-sm:text-lg max-phone:text-lg'>Sobre Mim</p>
+                <p className='font-black text-2xl max-mobile:text-xl max-phone:text-lg p-16  max-mobile:text-xl max-tablet:p-6 max-sm:text-lg max-phone:text-lg'>Sobre Mim</p>
             </div>
-            <div className='flex flex-col justify-between item-start space-y-24 content-center px-16 text-2xl max-mobile:text-xl max-sm:text-lg max-phone:text-sm'>
+            <div className='flex flex-col justify-between item-start space-y-24 content-center px-16 text-2xl max-mobile:text-xl max-sm:text-lg max-phone:text-sm max-mobile:py-10'>
                 <p className='text-center w-3/5 max-phone:w-full'>Atualmente estudo Fullstack, aprimorando minhas habilidades na criação de interfaces</p>
                 <p className='text-center w-3/5 ml-auto max-phone:w-full'>Atualmente estudo Fullstack, aprimorando minhas habilidades na criação de interfaces</p>
             </div>
--- src/pages/technologies.tsx
+++ src/pages/technologies.tsx
@@ -7,7 +7,7 @@ function Tech() {
         <div className="w-screen h-screen md:w-auto bg-secondary font-poppins text-primary p-5  max-phone:h-100 max-phone:rounded-t-3xl max-phone:mb-8px max-phone:shadow-phone">
 
 
-            <h1 className='font-black text-2xl p-14 max-mobile:text-xl max-phone:text-lg max-phone:p-6 max-phone:mb-8' >Técnologias</h1>
+            <h1 className='font-black text-2xl p-14 max-mobile:text-xl max-phone:text-lg max-phone:p-2 max-phone:mb-8' >Técnologias</h1>
 
             <div className='flex justify-center '>
                 <img src="Elipse.svg" alt="elipse" className='max-w-[450px] max-h-[450px]  max-mobile:max-w-[300px] max-mobile:max-h-[300px] max-phone:max-h-[250px] max-phone:max-h-[250px]' />
